e251c47810286248f43fb4a3b0925466387100c4
ValentynProtsyuk <113713025+ValentynProtsyuk@users.noreply.github.com>
2023-11-14 18:45:59 -0800

    Add files via upload

--- test.py
+++ test.py
@@ -1,35 +1,51 @@
 
-
 class Cache:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.cache = []  #declare cache data member
+        self.cache = []  # declare cache data member
         self.index = -1
-        
+
     def searchName(self, query):
         i = 0
         while i < len(self.cache):
             if self.cache[i].name == query:
                 return self.cache[i].val
-                #return True
-            else: 
+                # return True
+            else:
                 i += 1
 
         print("No RR by that name")
         return -1
-    
+
+    # Search based on name and type
+    # Returns whole row
+    def searchQuery(self, url, infType):
+        i = 0
+        while i < len(self.cache):
+            if (self.cache[i].name == url and self.cache[i].infoType == infType):
+                print("Query was found in the table!")
+                return self.cache[i]
+                # return True
+            else:
+                i += 1
+
+        print("Query not found in the table")
+        return -1
+
+
+
     def returnObject(self, query):
         i = 0
         while i < len(self.cache):
             if self.cache[i].name == query:
                 return self.cache[i]  # returns entire object this time
-                
-            else: 
+
+            else:
                 i += 1
 
         print("No Object by that Name")
         return -1
-        
+
 
 
     def getCache(self, query):
@@ -37,13 +53,13 @@ class Cache:
             return self.cache[query]
         else:
             return None
-        
+
     def searchCache(self, query):
         if query in self.cache:
             return True
         else:
             return False
-        
+
     def pushCache(self, val):
         if(len(self.cache) >= self.capacity):
             print("Cache is full")
@@ -54,16 +70,21 @@ class Cache:
             self.index += 1
 
     def delCache(self, val):
-        
+
         if val in self.cache:
 
             self.cache.remove(val)
             self.index -= 1
-            
+
         else:
             print("Element not found")
             return None
-                
+
+    def printCache(self):
+        for i in self.cache:
+            print(f"ID: {i.id}, Name: {i.name}, Type: {i.infoType}, Value: {i.val}"
+                  f", TTL:{i.ttl}, Static: {i.static}")
+
 
 class RR:
     def __init__(self, id, name, infoType, val, ttl, static):
@@ -75,24 +96,25 @@ class RR:
         self.static = static
         self.list = [id, name, infoType, val, ttl, static]
 
-    
+
     def printAll(self):
         print(self.list)
 
+    def assembleRR(newID ,importantInfoList, newTtl):
+        # current highest ID + 1 for a new entry ID
+        newID += 1
+        return RR(newID, importantInfoList[0], importantInfoList[1] ,importantInfoList[2], newTtl ,0)
 
 
-
-
-
-#'''           
+# '''
 test = Cache(10)
 
-test.pushCache(RR(1, "www.csusm.edu", 'A', "144.37.5.45", 60, 1))
-test.pushCache(RR(2, "cc.csusm.edu", 'A', "144.37.5.117", 60, 1))
-test.pushCache(RR(3, "cc1.csusm.edu", 'CNAME', "cc.csusm.edu", 60, 1))
-test.pushCache(RR(4, "cc1.csusm.edu", 'A', "144.37.5.118", 60, 1))
-test.pushCache(RR(5, "my.csusm.edu", 'A', "144.37.5.150", 60, 1))
-test.pushCache(RR(6, "qualcomm.com", "NS", "dns.qualcomm.com", 60, 1))
+test.pushCache(RR(1, "www.csusm.edu", 'A', "144.37.5.45", "", 1))
+test.pushCache(RR(2, "cc.csusm.edu", 'A', "144.37.5.117", "", 1))
+test.pushCache(RR(3, "cc1.csusm.edu", 'CNAME', "cc.csusm.edu", "", 1))
+test.pushCache(RR(4, "cc1.csusm.edu", 'A', "144.37.5.118", "", 1))
+test.pushCache(RR(5, "my.csusm.edu", 'A', "144.37.5.150", "", 1))
+test.pushCache(RR(6, "qualcomm.com", "NS", "dns.qualcomm.com", "", 1))
 
 '''
 testName = test.returnObject("my.csusm.edu")
@@ -102,23 +124,17 @@ else:
     print("It's -1")
 '''
 
-#'''
+# '''
 
 
+# test = Cache(5)
+# test.pushCache(RR(2, "cc.csusm.edu", 'A', "144.37.5.117", 60, 1))
+# test.pushCache(RR(6, "qualcomm.com", "NS", "dns.qualcomm.com", 60, 1))
 
-
-
-
-
-
-#test = Cache(5)
-#test.pushCache(RR(2, "cc.csusm.edu", 'A', "144.37.5.117", 60, 1))
-#test.pushCache(RR(6, "qualcomm.com", "NS", "dns.qualcomm.com", 60, 1))
-
-#testSearch = test.searchName("qualcomm.com")
-#if(testSearch != -1):
+# testSearch = test.searchName("qualcomm.com")
+# if(testSearch != -1):
 #    print(testSearch)
-#else:
+# else:
 #    print("testSearch has failed")
 
 
